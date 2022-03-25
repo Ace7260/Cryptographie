3f1d40d1e0fecb50d72e6c46e5ec72bf9939be76
Irakoze Eddy Nobel <83595479+Ace7260@users.noreply.github.com>
2022-03-25 22:34:34 +0200

    Add files via upload

---
+++ Modulo.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Modulo {
+    /*********Pour les Fractions a/bmodc***********/
+    static int ModuloNeg(int a, int b, int c){
+        int result=a;
+        if(a<b){
+            do{
+                      result=result+c;
+            }  while (!(result%b==0));
+            return result/b;
+        }else {
+            return 1;
+        }
+    }
+
+    static int ModuloPositif(int a, int b){
+        return a%b;
+    }
+    int ModuloPosi_Negtif(int a,int b,int c){
+
+        return 1;
+    }
+    int Modulo_Neg_Neg(int a,int b,int c){
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        /*forme general a/b mod c*/
+        System.out.println("1.Forme general  a/b mod c");
+        System.out.println("2.Forme general -a/b mod c");
+        System.out.println("3.Forme general -a mod c");
+        System.out.println("4.Forme general  a mod c\n");
+        System.out.println("Votre choix:");
+        if(IO.setINT("")==2){
+
+        System.out.print("Entrer a=");
+        int a= IO.setINT("");
+        System.out.print("Entrer b=");
+        int b= IO.setINT("");
+        System.out.print("Entrer c=");
+        int c= IO.setINT("");
+
+
+        int k=ModuloNeg(a,b,c);
+        if(k==0||k<0){
+            System.out.print("impossible");
+        }else{
+            System.out.println("Modulo=> "+a+"/"+b+"Mod"+c+"="+k);
+        }}
+        else if(IO.setINT("")==4){
+            System.out.print("Entrer a=");
+            int a= IO.setINT("");
+            System.out.print("Entrer b=");
+            int b= IO.setINT("");
+            int k=ModuloPositif(a,b);
+            System.out.println("Modulo=> "+a+"Mod"+b+"="+k);
+            }
+        else{
+            System.out.println("Pas encore");
+        }
+    }
+}
